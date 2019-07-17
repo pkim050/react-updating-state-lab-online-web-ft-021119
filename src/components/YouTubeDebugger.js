ddc316cce18465ffbefe30d9990a0dfd20206c68
Patrick Kim <pkim050@gmail.com>
2019-07-17 10:46:45 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -31,7 +31,7 @@ export default class YouTubeDebugger extends React.Component {
         ...this.state.settings,
         video: {
           ...this.state.settings.video,
-          testing: "Hi"
+          resolution: '720p'
         }
       }
     })
