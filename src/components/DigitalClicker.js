91dab80c40ef150c47d45e41ceac26c43c621981
Patrick Kim <pkim050@gmail.com>
2019-07-17 10:23:24 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,5 +2,24 @@
 import React from 'react';
 
 export default class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
 
+  handleClick = () => {
+    this.setState({
+      timesClicked: timesClicked++
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
 }
